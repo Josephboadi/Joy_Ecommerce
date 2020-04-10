a76df26965714e338cee614f467790b2822b8fe8
Josephboadi <boadijoseph151@gmail.com>
2020-04-10 17:23:41 +0200

    Third commit

--- joyecommerce/settings/base.py
+++ joyecommerce/settings/base.py
@@ -81,6 +81,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static_root')
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media_root')
 
 
+
 AUTHENTICATION_BACKENDS = (
     'django.contrib.auth.backends.ModelBackend',
     'allauth.account.auth_backends.AuthenticationBackend',
